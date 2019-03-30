2e7fef3745a7b154a53bbfbd6414571dc92d6def
Mark Szabo <a-marks@microsoft.com>
2019-03-30 23:13:27 +0100

    Adding Class Library project

---
+++ MyNewHome.ClassLibrary/Pet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNewHome.ClassLibrary
+{
+    public class Pet
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string ContactEmail { get; set; }
+    }
+}
